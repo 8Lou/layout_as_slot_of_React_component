0627e72be799be27fb9dad9813c28fb90451ffd9
8Lou <lou8@mail.ru>
2024-01-31 21:24:48 +0300

    create React start

---
+++ README.md
@@ -0,0 +1,4 @@
+# https://8Lou.github.io/layout_as_slot_of_React_component
+
+Getting Started with Create React App
+### `npm start`
---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+
+function App() {
+  return (
+    <div className="">
+      <p>
+        layout_as_slot_of_React_component
+      </p>
+
+    </div>
+  );
+}
+
+export default App;

d1fc0eb487bfb420272993c24bcc90e8f33077eb
8Lou <lou8@mail.ru>
2024-02-02 09:09:58 +0300

    edit a location

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -2,23 +2,24 @@ import React, { useRef, useEffect } from 'react';
 
 const Map = () => {
     const mapRef = useRef();
+    const location = { lat: 40.73061, lng: -73.935242 }; // Координаты точки местоположения
 
     useEffect(() => {
         const google = window.google;
         const map = new google.maps.Map(mapRef.current, {
-            center: { lat: -34.397, lng: 150.644 },
-            zoom: 8,
+            center: location,
+            zoom: 12, //Чем выше значение `zoom`, тем более детально
         });
 
         const marker = new google.maps.Marker({
-            position: { lat: -34.397, lng: 150.644 },
+            position: location,
             map: map,
-            title: 'Hello World!'
+            title: 'Obninsk'
         });
 
         // Обновляем размер карты при изменении размеров окна
         window.addEventListener('resize', () => {
-            map.setCenter({ lat: -34.397, lng: 150.644 });
+            map.setCenter(location);
         });
 
         return () => {
@@ -26,7 +27,7 @@ const Map = () => {
                 map.setCenter({ lat: -34.397, lng: 150.644 });
             });
         };
-    }, []);
+    }, [location]);
 
     return <div ref={mapRef} style={{ width: '100%', height: '100vh' }}></div>;
 };
